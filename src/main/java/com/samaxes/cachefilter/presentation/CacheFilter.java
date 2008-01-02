0deb0a2b7c658611b4d670221999dd0b7bb46bdd
Samuel Santos <samaxes@gmail.com>
2008-01-02 05:40:59 +0000

    Update svn:keywords

--- src/main/java/com/samaxes/cachefilter/presentation/CacheFilter.java
+++ src/main/java/com/samaxes/cachefilter/presentation/CacheFilter.java
@@ -1,8 +1,19 @@
 /*
- * $Id$
+ * $Id$ 
  *
- * Last changed on : $Date$
- * Last changed by : $Author$
+ * Copyright 2008 samaxes.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.samaxes.cachefilter.presentation;
 
@@ -31,7 +42,7 @@ import org.slf4j.LoggerFactory;
  * Filter responsible for browser caching.
  * 
  * @author : Samuel Santos
- * @version : $Revision$
+ * @version : $LastChangedRevision$
  */
 public class CacheFilter implements Filter {
 
