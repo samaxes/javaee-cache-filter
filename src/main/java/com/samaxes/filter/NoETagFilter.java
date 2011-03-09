c501e1df32d9b3a7db1a95ad841d49057a49388e
Samuel Santos <samaxes@gmail.com>
2011-03-09 02:49:17 +0000

    Refactoring, changed filter's name

---
+++ src/main/java/com/samaxes/filter/NoETagFilter.java
@@ -0,0 +1,74 @@
+/*
+ * $Id$
+ *
+ * Copyright 2011 samaxes.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.samaxes.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+import com.samaxes.filter.util.HTTPCacheHeader;
+
+/**
+ * Filter responsible for disabling ETag header from the HTTP response.
+ * 
+ * @author Samuel Santos
+ * @version $Revision$
+ */
+public class NoETagFilter implements Filter {
+
+    /**
+     * Place this filter into service.
+     * 
+     * @param filterConfig the filter configuration object used by a servlet container to pass information to a filter
+     *        during initialization
+     * @throws ServletException to inform the container to not place the filter into service
+     */
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    /**
+     * Disables ETag HTTP header.
+     * 
+     * @param servletRequest provides data including parameter name and values, attributes, and an input stream
+     * @param servletResponse assists a servlet in sending a response to the client
+     * @param filterChain gives a view into the invocation chain of a filtered request
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException {
+        chain.doFilter(request, new HttpServletResponseWrapper((HttpServletResponse) response) {
+            public void setHeader(String name, String value) {
+                if (!HTTPCacheHeader.ETAG.getName().equalsIgnoreCase(name)) {
+                    super.setHeader(name, value);
+                }
+            }
+        });
+    }
+
+    /**
+     * Take this filter out of service.
+     */
+    public void destroy() {
+    }
+}
